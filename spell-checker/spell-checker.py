060fe3fd018e48c757dd7cb15d25bcc80299a79e
Tasmiya-Malik <60136068+Tasmiya-Malik@users.noreply.github.com>
2023-04-09 17:02:52 +0500

    indicators 1, 3, 6, 10

---
+++ spell-checker/spell-checker.py
@@ -0,0 +1,50 @@
+import csv
+import os
+import pandas as pd
+
+#functions
+def dataset(folder):
+    s_list = list()
+    tgt_file = 'C:/Users/user/OneDrive/University/08. Spring2023/NLP/Project/nlp-spelling-dysign/data.csv'
+    for x in os.listdir(folder):
+
+        with open(folder+x, mode='r', newline='\n') as file:
+            line = file.readlines()
+            # print(id, line[0])
+            s_list.append([x[:-4], line[0]])
+    with open(tgt_file, mode='w', newline='\n') as file:
+        writer = csv.writer(file)
+        for x in s_list:
+            writer.writerow(x)
+def scoring(df):
+    print(df['sentence'])
+folder1 = 'C:/Users/user/OneDrive/University/08. Spring2023/NLP/Project/nlp-spelling-dysign/data-transcibed-copy/'
+data = 'C:/Users/user/OneDrive/University/08. Spring2023/NLP/Project/nlp-spelling-dysign/data.csv'
+
+# dataset(folder1)   #creating a csv
+word_dict = {
+    1: 'the',
+    2: 'beautiful',
+    3: 'ducks',
+    4: 'swam',
+    5: 'in',
+    6: 'big',
+    7: 'blue',
+    8: 'lake',
+    9: 'their',
+    10: 'feathers',
+    11: 'were',
+    12: 'very',
+    13: 'pretty',
+    14: 'and',
+    15: 'quite',
+    16: 'often',
+    17: 'would',
+    18: 'jump',
+    19: 'out',
+    20: 'of',
+    21: 'water',
+}
+
+df = pd.read_csv(data)
+scoring(df)
